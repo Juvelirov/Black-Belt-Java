824ac1c124108159cbd4f3f12f8c2aaee5863238
Artem Denisov <denisov_today@mail.ru>
2025-01-20 01:03:33 +0500

    learned comparable interface

---
+++ src/comporation/Test1.java
@@ -0,0 +1,20 @@
+package comporation;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Test1 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Zaur");
+        list.add("Ivan");
+        list.add("Maria");
+        System.out.println("before sorting: ");
+        System.out.println(list);
+        System.out.println("after sorting: ");
+        Collections.sort(list);
+        System.out.println(list);
+    }
+}
