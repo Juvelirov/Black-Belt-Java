aadc38050dadc99dd34a7f392f43cbf2577222c4
Artem Denisov <denisov_today@mail.ru>
2025-01-31 21:19:41 +0500

    learned stack methods

---
+++ src/syntax_pro/collections/StackMethods.java
@@ -0,0 +1,20 @@
+package syntax_pro.collections;
+
+import java.util.Stack;
+
+public class StackMethods {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        stack.push("Zaur");
+        stack.push("Misha");
+        stack.push("Oleg");
+        stack.push("Katya"); // push вставляет в верхушку
+        System.out.println("До pop(): " + stack);
+        System.out.println(stack.pop());
+        System.out.println("После pop(): " + stack);
+        while (!stack.isEmpty()){
+            System.out.println(stack.pop());
+            System.out.println(stack);
+        }
+    }
+}
